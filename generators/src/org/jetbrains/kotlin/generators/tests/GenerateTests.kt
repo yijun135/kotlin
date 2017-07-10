25e6ba1a904f1290381c94dc6ddb4bfb22b570d8
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-07-10 22:57:14 +0300

    Add inspection for usage of Android Extension property from wrong layout
     #KT-17607 Fixed

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1331,6 +1331,10 @@ fun main(args: Array<String>) {
         testClass<AbstractAndroidExtractionTest> {
             model("android/extraction", recursive = false, extension = null)
         }
+
+        testClass<AbstractAndroidExtensionsInspectionTest> {
+            model("android/inspection", singleClass = true)
+        }
     }
 
     testGroup("idea/idea-android/tests", "idea/testData") {
