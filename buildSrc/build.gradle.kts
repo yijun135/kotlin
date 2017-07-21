1064896f2ad12e737c695791e3a534c13991f192
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-07-21 15:22:15 +0200

    Switch to the fresh eap as a bootstrap

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,13 +1,25 @@
 buildscript {
 
+    extra["repos"] = listOf(
+            "https://dl.bintray.com/kotlin/kotlin-eap",
+            "https://dl.bintray.com/kotlin/kotlin-dev",
+            "https://repo.gradle.org/gradle/repo",
+            "https://plugins.gradle.org/m2")
+
     project.extra["kotlinVersion"] = file("../kotlin-bootstrap-version.txt").readText().trim()
 
     configure(listOf(repositories, project.repositories)) {
-        gradleScriptKotlin()
+        gradleKotlinDsl()
+    }
+
+    repositories {
+        for (repo in (rootProject.extra["repos"] as List<String>)) {
+            maven { setUrl(repo) }
+        }
     }
 
     dependencies {
-        classpath(kotlinModule("gradle-plugin", version = project.extra["kotlinVersion"] as String))
+        classpath(kotlin("gradle-plugin", version = project.extra["kotlinVersion"] as String))
     }
 }
 
@@ -15,9 +27,15 @@ apply {
     plugin("kotlin")
 }
 
+repositories {
+    for (repo in (rootProject.extra["repos"] as List<String>)) {
+        maven { setUrl(repo) }
+    }
+}
+
 dependencies {
     val compile by configurations
-    compile(kotlinModule("stdlib", version = project.extra["kotlinVersion"] as String))
+    compile(kotlin("stdlib", version = project.extra["kotlinVersion"] as String))
     compile(gradleApi())
-    compile(gradleScriptKotlinApi())
+    compile(gradleKotlinDsl())
 }
